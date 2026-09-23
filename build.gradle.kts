3757b18523c97cf137911b18a426df18cc65c964
MarcPG <me@marcpg.com>
2026-09-23 21:17:01 +0200

    Forge Integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
     mavenCentral()
 
     maven("https://maven.fabricmc.net/")
+    maven("https://maven.minecraftforge.net")
     maven("https://maven.neoforged.net/releases/")
     maven("https://repo.faststats.dev/releases")
 }
@@ -38,6 +39,7 @@ dependencies {
     minecraft(libs.minecraft)
 
     compileOnly(libs.bundles.fabric)
+    compileOnly(libs.bundles.forge)
     compileOnly(libs.bundles.neoforge)
 }
 
---
+++ src/main/java/com/marcpg/faststats/compat/implementations/ForgeCompat.java
@@ -0,0 +1,68 @@
+package com.marcpg.faststats.compat.implementations;
+
+import com.marcpg.faststats.compat.Compat;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.loading.FMLPaths;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
+import org.jetbrains.annotations.Nullable;
+
+public class ForgeCompat extends Compat {
+    private @Nullable MinecraftServer server = null;
+
+    public ForgeCompat(String modId) {
+        this.platformVersion = ModList.getModContainerById("forge")
+                .map(container -> container.getModInfo().getVersion().toString())
+                .orElse("unknown");
+        this.configDir = FMLPaths.CONFIGDIR.get();
+
+        ModContainer mod = ModList.getModContainerById(modId).orElseThrow(() -> new IllegalArgumentException("No mod found with ID: " + modId));
+        this.modName = mod.getModInfo().getDisplayName();
+        this.modVersion = mod.getModInfo().getVersion().toString();
+
+        this.isClient = FMLLoader.getDist() == Dist.CLIENT;
+
+        if (!this.isClient)
+            waitForServer(() -> this.server = ServerLifecycleHooks.getCurrentServer());
+    }
+
+    @Override
+    protected MinecraftServer server() {
+        assert server != null : "Server not initialized";
+        return server;
+    }
+
+    @Override
+    public void registerLifecycleEvents(Runnable ready, Runnable shutdown) {
+        if (isClient) {
+            ready.run();
+        } else {
+            waitForServer(ready);
+        }
+        // Because we can't use goofy forge events (super volatile event API for some reason), we just hook straight into the JVM:
+        Runtime.getRuntime().addShutdownHook(new Thread(shutdown));
+    }
+
+    private void waitForServer(Runnable ready) {
+        Thread.startVirtualThread(() -> {
+            // Require 2 encounters to give the server some time (1-2 seconds) to actually start:
+            int availableCounter = 0;
+            while (2 > availableCounter) {
+                if (ServerLifecycleHooks.getCurrentServer() != null)
+                    availableCounter++;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+            }
+
+            server = ServerLifecycleHooks.getCurrentServer();
+            ready.run();
+        });
+    }
+}
